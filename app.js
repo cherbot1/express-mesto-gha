a42fa502fecb98ea5c3b2983ae63324008695d61
Oleg <cherbot@mail.ru>
2022-07-27 17:42:11 +0300

    troubles with server

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+app.use('/users', require('./routes/users'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
