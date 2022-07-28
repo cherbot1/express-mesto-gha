b3325cb130f7fda8a8559893fa66ecad12e58521
Oleg <cherbot@mail.ru>
2022-07-28 17:40:56 +0300

    finally fixed?

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const NOT_FOUND = 404;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,5 +24,9 @@ app.use((req, res, next) => {
 
 app.use('/cards', require('./routes/cards'));
 app.use('/users', require('./routes/users'));
+app.use('/', (req, res) => {
+  res
+    .status(NOT_FOUND).send({ message: 'Страницы не существует' });
+});
 
 app.listen(PORT);
