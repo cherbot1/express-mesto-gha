51851d286606aa0ef10d942407f261d1bdf9ad94
Oleg <cherbot@mail.ru>
2022-08-10 16:58:52 +0300

    regex pattern 2

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.post('/signup', celebrate({
     password: Joi.string()
       .required(),
     avatar: Joi.string()
-      .regex(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/),
+      .regex(/https?:\/\/(www)?[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]+#?$/i),
   }).unknown(true),
 }), createUser);
 
