932c4439b7d29c0f9cbbfabee14654fa38f0c5b7
Oleg <cherbot@mail.ru>
2022-08-12 13:16:14 +0300

    card delete fix vol.2

--- controllers/cards.js
+++ controllers/cards.js
@@ -40,7 +40,7 @@ module.exports.deleteCard = (req, res, next) => {
       throw new NotFoundError('Карточки не существует');
     })
     .then((card) => {
-      if (req.user._id !== card.owner._id.valueOf()) {
+      if (req.user._id !== card.owner.id) {
         return next(new ForbiddenError('Карточка создана не Вами, удалить невозможно'));
       }
       return Card.deleteOne(card)
